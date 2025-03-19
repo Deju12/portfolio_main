dc287dd052ade30b48e5241a639c26b7c33aa373
Dejene Tesfaye <110309987+Deju12@users.noreply.github.com>
2025-03-19 16:08:04 +0300

    Update my documents

--- components/Common/Intro/Download.jsx
+++ components/Common/Intro/Download.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 const Download = ({ icon }) => {
   return (
     <>
-      <Link href='https://drive.google.com/file/d/1SZLOKI-ZfKhY34_OvDJI9PKaQEiNk_BX/view' target='_blank' className='flex flex-row text-LightGray items-center gap-x-4 pb-14 pt-4'>
+      <Link href='https://drive.google.com/file/d/1dZ30W1Dz9uS59QQBzTTe5CovQJ979Xvz/view?usp=sharing' target='_blank' className='flex flex-row text-LightGray items-center gap-x-4 pb-14 pt-4'>
         <span className='text-Snow'>Download Resume</span>
         <span>{icon}</span>
       </Link>
